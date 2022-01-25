4115376b11f95b74fa3f5d6731672b148b4e4ec7
gh0$t <6196046+g-h-0-S-t@users.noreply.github.com>
2022-01-26 01:12:27 +0530

    Update ExtractFBGroupMembers.js
    optimisation

--- ExtractFBGroupMembers.js
+++ ExtractFBGroupMembers.js
@@ -22,10 +22,8 @@ javascript:
      * SOFTWARE.*/
     'use strict';
 (function(window) {
-    var tab = window.open('about:blank', '_blank');
-    var count = 0;
-    tab.document.write('<style>*{background-color:#0a0a0a;color:#f0f0f0;font-family:"Gill Sans", sans-serif;text-align: center;margin:0px auto;} a{color:#c3015c;}</style><h1>Facebook Group Members\' Profile Extractor by <a target = "_blank" href = "https://github.com/g-h-0-S-t">gh0$t</a><br>Get the tool <a target = "_blank" href = "https://github.com/g-h-0-S-t/ExtractFBGroupMembers">here</a><hr>Group Name : <a target = "_blank" href = "' + window.location.href + '">' + window.document.querySelectorAll('a[href*="/groups/"]')[2].text + '</a></h1><h2 id="action">Group Members (Total : <span id = "count"></span>)</h2><hr>');
-    tab.document.write('<table id = "theList" border="1" cellspacing="0" cellpadding="10"><thead><tr><th>Serial Number</th><th>Profile Picture</th><th>Profile Name</th><th>Profile Details</th><th>Profile URL</th></tr></thead><tbody>');
+    var tab = window.open('about:blank', '_blank'), count = 0;
+    tab.document.write('<style>*{background-color:#0a0a0a;color:#f0f0f0;font-family:"Gill Sans", sans-serif;text-align: center;margin:0px auto;} a{color:#c3015c;}</style><h1>Facebook Group Members\' Profile Extractor by <a target = "_blank" href = "https://github.com/g-h-0-S-t">gh0$t</a><br>Get the tool <a target = "_blank" href = "https://github.com/g-h-0-S-t/ExtractFBGroupMembers">here</a><hr>Group Name : <a target = "_blank" href = "' + window.location.href + '">' + window.document.querySelectorAll('a[href*="/groups/"]')[2].text + '</a></h1><h2 id="action">Group Members (Total : <span id = "count"></span>)</h2><hr><table id = "theList" border="1" cellspacing="0" cellpadding="10"><thead><tr><th>Serial Number</th><th>Profile Picture</th><th>Profile Name</th><th>Profile Details</th><th>Profile URL</th></tr></thead><tbody>');
     window.document.querySelectorAll('[class="ow4ym5g4 auili1gw rq0escxv j83agx80 buofh1pr g5gj957u i1fnvgqd oygrvhab cxmmr5t8 hcukyx3x kvgmc6g5 hpfvmrgz qt6c0cv9 jb3vyjys l9j0dhe7 du4w35lb bp9cbjyn btwxx1t3 dflh9lhu scb9dxdr nnctdnn4"]').forEach(function(v, i, a) {
         if (!v.textContent.toLowerCase().includes('invited') && v.textContent !== window.document.querySelectorAll('a[href*="/groups/"]')[2].text && !tab.document.getElementById('theList').innerHTML.includes(v.querySelector('a').href)) {
             count += 1;
