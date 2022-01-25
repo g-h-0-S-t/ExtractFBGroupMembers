37c3cbf89b6934f90a35924ffb9d6b87b333a618
gh0$t <6196046+g-h-0-S-t@users.noreply.github.com>
2022-01-26 00:42:28 +0530

    Update ExtractFBGroupMembers.js
    optimisation

--- ExtractFBGroupMembers.js
+++ ExtractFBGroupMembers.js
@@ -26,19 +26,10 @@ javascript:
     var count = 0;
     tab.document.write('<style>*{background-color:#0a0a0a;color:#f0f0f0;font-family:"Gill Sans", sans-serif;text-align: center;margin:0px auto;} a{color:#c3015c;}</style><h1>Facebook Group Members\' Profile Extractor by <a target = "_blank" href = "https://github.com/g-h-0-S-t">gh0$t</a><br>Get the tool <a target = "_blank" href = "https://github.com/g-h-0-S-t/ExtractFBGroupMembers">here</a><hr>Group Name : <a target = "_blank" href = "' + window.location.href + '">' + window.document.querySelectorAll('a[href*="/groups/"]')[2].text + '</a></h1><h2 id="action">Group Members (Total : <span id = "count"></span>)</h2><hr>');
     tab.document.write('<table id = "theList" border="1" cellspacing="0" cellpadding="10"><thead><tr><th>Serial Number</th><th>Profile Picture</th><th>Profile Name</th><th>Profile Details</th><th>Profile URL</th></tr></thead><tbody>');
-    window.document.querySelectorAll('a[href*="/groups/"][href*="/user/"]').forEach(function(v, i, a) {
-        if (!tab.document.getElementById('theList').innerHTML.includes(v.href)) {
-            if (!v.text && v.querySelector('image')) {
-                count += 1;
-                tab.document.write('<tr>');
-                tab.document.write('<td>' + count + '</td><td><img height = "38" width = "38" src = "' + v.querySelector('image').getAttribute('xlink:href') + '"/></td>');
-            }
-            if (v.text) {
-                try {
-                    tab.document.write('<td><a target = "_blank" href ="' + v.href + '">' + v.text + '</a></td><td><a target = "_blank" href ="' + v.href + '">' + v.closest('.qzhwtbm6.knvmm38d').nextSibling.textContent + '<br>' + v.closest('.qzhwtbm6.knvmm38d').nextSibling.nextSibling.textContent + '</a></td><td><a target = "_blank" href ="' + v.href + '">' + v.href + '</a></td>');
-                } catch { /** meh, whatever */ }
-                tab.document.write('</tr>');
-            }
+    window.document.querySelectorAll('[class="ow4ym5g4 auili1gw rq0escxv j83agx80 buofh1pr g5gj957u i1fnvgqd oygrvhab cxmmr5t8 hcukyx3x kvgmc6g5 hpfvmrgz qt6c0cv9 jb3vyjys l9j0dhe7 du4w35lb bp9cbjyn btwxx1t3 dflh9lhu scb9dxdr nnctdnn4"]').forEach(function(v, i, a) {
+        if (!v.textContent.toLowerCase().includes('invited') && v.textContent !== window.document.querySelectorAll('a[href*="/groups/"]')[2].text) {
+            count += 1;
+            tab.document.write('<tr><td>' + count + '</td><td><img height = "38" width = "38" src = "' + v.querySelector('image').getAttribute('xlink:href') + '"/></td><td><a target = "_blank" href ="' + v.querySelector('a').href + '">' + v.querySelector('[class = "j83agx80 cbu4d94t ew0dbk1b irj2b8pg"] > [class = "qzhwtbm6 knvmm38d"]:nth-child(1)').textContent + '</a></td><td><a target = "_blank" href ="' + v.querySelector('a').href + '">' + v.querySelector('[class = "j83agx80 cbu4d94t ew0dbk1b irj2b8pg"] > [class = "qzhwtbm6 knvmm38d"]:nth-child(2)').textContent + '<br>' + v.querySelector('[class = "j83agx80 cbu4d94t ew0dbk1b irj2b8pg"] > [class = "qzhwtbm6 knvmm38d"]:nth-child(3)').textContent + '</a></td><td><a target = "_blank" href ="' + v.querySelector('a').href + '">' + v.querySelector('a').href + '</a></td></tr>');
         }
     });
     tab.document.write('</tbody></table>');
